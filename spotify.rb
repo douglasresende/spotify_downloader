85af1bbf78ba79ca4082c5b9029013475f073fe8
Marcelo Junior <marcelo.jr63@gmail.com>
2018-10-05 07:45:13 -0300

    Update spotify.rb

--- spotify.rb
+++ spotify.rb
@@ -14,9 +14,9 @@ require_relative 'src/spotify_downloader'
 
 config = {
   download_path: '~/Music/',
-  spotify_token: 'BQCfpUC7gnGgoxSxvtUMThLA96PI3Ly-rULnIFharAZOoFMenT9jauF_FBloBH6aiB3IVYESUF_qnmy-ZHOnml95u0WpBjoi4xJT85adxCs4QdomMlPOJbC88eCKMkD50gpNWbLKjG0ILtidlD4',
-  google_token:  'AIzaSyAv0itwIjlbcTNaEzVH4ai6SH5fqhDx0S8'
+  spotify_token: 'SPOTIFY-TOKEN',
+  google_token:  'YOUTUBE-TOKEN'
 }
 
 client = SpotifyDownloader.new(config)
-client.download!('37i9dQZF1DWSIvbYYt1Dvi')
+client.download!('PLAYLIST-ID')
